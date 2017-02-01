745623b23bf2f8202e10bd556dcc44cf6c817ddf
Sebastian Wijkhuizen <sebastianwijkhuizen@gmail.com>
2017-02-01 19:38:33 +0100

    add touch method declaration

--- src/adapters/CacheAdapter.ts
+++ src/adapters/CacheAdapter.ts
@@ -1,8 +1,11 @@
 export type ResultCallback = (error: Error, result: any) => void;
 
 abstract class CacheAdapter {
+    constructor(public expire: number) { }
+
     abstract get(key: string, callback: ResultCallback): void;
     abstract set(key: string, value: string, callback: ResultCallback): void;
+    abstract touch(key: string, callback: ResultCallback): void;
 }
 
 export default CacheAdapter;
