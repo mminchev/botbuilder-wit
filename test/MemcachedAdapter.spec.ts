18cd0ba23b2ccd3124739e73ecd4864ee74d77bf
Sebastian Wijkhuizen <sebastianwijkhuizen@gmail.com>
2017-02-01 19:39:40 +0100

    add test for touch method

--- test/MemcachedAdapter.spec.ts
+++ test/MemcachedAdapter.spec.ts
@@ -1,35 +1,50 @@
 import { expect } from 'chai';
 const MemcachedAdapter = require('../lib/adapters/MemcachedAdapter').default;
 
-describe('MemcachedAdapter', function() {
-    describe('MemcachedAdapter#get()', function() {
+describe('MemcachedAdapter', function () {
+    describe('MemcachedAdapter#get()', function () {
         it('should invoke the get method of the Memcached client with the right arguments', function () {
             // Create Memcached client mock
-            function Client() {}
+            function Client() { }
             Client.prototype.get = function () {
                 expect(arguments[0]).to.equal('key');
                 expect(arguments[1]).to.be.a('function');
             };
-            
+
             const client = new Client();
             const adapter = new MemcachedAdapter(client, 3600);
-            adapter.get('key', () => {});
+            adapter.get('key', () => { });
         });
     });
-    describe('MemcachedAdapter#set()', function() {
+    describe('MemcachedAdapter#set()', function () {
         it('should invoke the set method of the Memcached client with the right arguments', function () {
             // Create Memcached client mock
-            function Client() {}
+            function Client() { }
             Client.prototype.set = function () {
                 expect(arguments[0]).to.equal('key');
                 expect(arguments[1]).to.equal('value');
                 expect(arguments[2]).to.equal(3600);
                 expect(arguments[3]).to.be.a('function');
             };
-            
+
             const client = new Client();
             const adapter = new MemcachedAdapter(client, 3600);
-            adapter.set('key', 'value', () => {});
+            adapter.set('key', 'value', () => { });
+        });
+    });
+    describe('MemcachedAdapter#touch()', function () {
+        it('should invoke the set method of the Memcached client with the right arguments', function () {
+            // Create Memcached client mock
+            function Client() { }
+            Client.prototype.touch = function () {
+                expect(arguments[0]).to.equal('key');
+                expect(arguments[1]).to.equal(3600);
+                expect(arguments[2]).to.be.a('function');
+            };
+
+            const client = new Client();
+            const adapter = new MemcachedAdapter(client, 3600);
+            adapter.touch('key', () => { });
         });
     });
 });
--- test/RedisAdapter.spec.ts
+++ test/RedisAdapter.spec.ts
@@ -1,25 +1,25 @@
 import { expect } from 'chai';
 const RedisAdapter = require('../lib/adapters/RedisAdapter').default;
 
-describe('RedisAdapter', function() {
-    describe('RedisAdapter#get()', function() {
+describe('RedisAdapter', function () {
+    describe('RedisAdapter#get()', function () {
         it('should invoke the get method of the Redis client with the right arguments', function () {
             // Create Redis client mock
-            function RedisClient() {}
+            function RedisClient() { }
             RedisClient.prototype.get = function () {
                 expect(arguments[0]).to.equal('key');
                 expect(arguments[1]).to.be.a('function');
             };
-            
+
             const client = new RedisClient();
             const adapter = new RedisAdapter(client, 3600);
-            adapter.get('key', () => {});
+            adapter.get('key', () => { });
         });
     });
-    describe('RedisAdapter#set()', function() {
+    describe('RedisAdapter#set()', function () {
         it('should invoke the set method of the Redis client with the right arguments', function () {
             // Create Redis client mock
-            function RedisClient() {}
+            function RedisClient() { }
             RedisClient.prototype.set = function () {
                 expect(arguments[0]).to.equal('key');
                 expect(arguments[1]).to.equal('value');
@@ -27,10 +27,25 @@ describe('RedisAdapter', function() {
                 expect(arguments[3]).to.equal(3600);
                 expect(arguments[4]).to.be.a('function');
             };
-            
+
             const client = new RedisClient();
             const adapter = new RedisAdapter(client, 3600);
-            adapter.set('key', 'value', () => {});
+            adapter.set('key', 'value', () => { });
+        });
+    });
+    describe('RedisAdapter#touch()', function () {
+        it('should invoke the set method of the Redis client with the right arguments', function () {
+            // Create Redis client mock
+            function RedisClient() { }
+            RedisClient.prototype.expire = function () {
+                expect(arguments[0]).to.equal('key');
+                expect(arguments[1]).to.equal(3600);
+                expect(arguments[2]).to.be.a('function');
+            };
+
+            const client = new RedisClient();
+            const adapter = new RedisAdapter(client, 3600);
+            adapter.touch('key', () => { });
         });
     });
 });
