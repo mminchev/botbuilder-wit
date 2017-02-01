4edf0a3edae546b5ce43e7662d1b279fa67af0a3
Sebastian <sebastian@furonto.com>
2017-02-01 19:39:07 +0100

    add touch method implementation

--- src/adapters/RedisAdapter.ts
+++ src/adapters/RedisAdapter.ts
@@ -3,7 +3,7 @@ import { ResultCallback } from './CacheAdapter';
 
 export default class RedisAdapter extends CacheAdapter {
     constructor(public redisClient: any, public expire: number) {
-        super();
+        super(expire);
     }
 
     /**
@@ -23,4 +23,13 @@ export default class RedisAdapter extends CacheAdapter {
     set(key: string, value: string, callback: ResultCallback) {
         this.redisClient.set(key, value, 'EX', this.expire, callback);
     }
+
+    /**
+     * Implements the CacheAdapter's abstract touch method
+     * @param {string} key - the value of the key of which the TTL will be reset
+     * @param {function} callback - the result callback
+     */
+    touch(key: string, callback: ResultCallback) {
+        this.redisClient.expire(key, this.expire, callback);
+    };
 }
