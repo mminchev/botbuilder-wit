6ecff038bc7f10b3857ae6cff7cd31ce10585f37
Sebastian <sebastian@furonto.com>
2018-01-09 11:26:07 +0100

    Extract types, fix linting errors

--- src/WitRecognizer.ts
+++ src/WitRecognizer.ts
@@ -1,76 +1,20 @@
-import { Wit, WitContext } from 'node-wit';
-import * as crypto from 'crypto';
-import CacheAdapter from './adapters/CacheAdapter';
-import RedisAdapter from './adapters/RedisAdapter';
-import MemcachedAdapter from './adapters/MemcachedAdapter';
-
-// Type of the object that represents an intent that is part of WitRecognizer's result
-export interface IIntent {
-    intent: string;
-    score: number;
-}
-
-// Type of the object(s) that represents an entity that is part of WitRecognizer's result
-export interface IEntity {
-    entity: string;
-    // The rawEntity property is included because Wit.ai doesn't have a perfectly consistent JSON response object.
-    // In most cases there is a "value" property whose string value is then assigned to entity.
-    // However, in case the value property is undefined, entity will be null.
-    // In that case, it's necessary to be able to access the original entity via the "rawEntity" property.
-    rawEntity: Object;
-    type: string;
-    startIndex?: number;
-    endIndex?: number;
-    score?: number;
-}
-
-// Type of the first argument of the recognize method. Contains the message text.
-export interface IRecognizeContext {
-    message: { text: string };
-}
-
-// Type of the second argument of the recognize method. Contains the processed result.
-export interface IIntentRecognizerResult {
-    score: number;
-    intent: string;
-    intents?: IIntent[];
-    entities?: IEntity[];
-}
-
-// NOTE: there is an important difference between IEntity and IWitEntity.
-// IEntity is the type of entity the Bot Builder SDK expects to receive from the recognize method.
-// Entities of type IWitEntity are contained in the JSON response received from Wit.ai.
-interface IWitEntity {
-    type?: string;
-    confidence: number;
-    value?: string;
-    values?: Object[];
-}
-
-// Type of the object that represents the JSON response from Wit.ai.
-interface IWitResults {
-    msg_id?: string;
-    _text?: string;
-    entities?: {
-        [index: string]: IWitEntity[];
-    };
-    error?: string;
-}
-
-export interface IOptions {
-    cache?: any;
-    expire?: number
-}
-
-export enum CacheClients {
-    Unknown, // 0
-    Redis,
-    Memcached,
-}
+import { createHash } from "crypto";
+import { MessageResponse, Wit, WitContext } from "node-wit";
+import CacheAdapter from "./adapters/CacheAdapter";
+import MemcachedAdapter from "./adapters/MemcachedAdapter";
+import RedisAdapter from "./adapters/RedisAdapter";
+import {
+    CacheClients,
+    IEntity,
+    IIntentRecognizerResult,
+    IOptions,
+    IRecognizeContext,
+    IWitResults,
+    MessageHandler,
+} from "./types";
 
 /**
  * The WitRecognizer class that is used in conjunction with an IntentDialog.
- * @class
  * Example:
  * const recognizer = new WitRecognizer('Wit.ai_access_token');
  * const dialog = new IntentDialog({recognizers: [recognizer]});
@@ -82,13 +26,14 @@ export class WitRecognizer {
     /**
      * Creates an instance of WitRecognizer.
      * @param accessToken {string} API token from Wit.ai
-     * @param cache {any} Redis or Memcached client
+     * @param options {IOptions}
      */
     constructor(accessToken: string, options: IOptions = {}) {
         const { cache } = options;
 
-        if (!accessToken || typeof accessToken !== 'string') {
-            throw new Error('Invalid argument. Constructor must be invoked with an accessToken of type "string".');
+        if (!accessToken || typeof accessToken !== "string") {
+            const msg = "Constructor must be invoked with an accessToken of type \"string\"";
+            throw new Error(msg);
         }
         this.witClient = new Wit({ accessToken });
 
@@ -96,7 +41,7 @@ export class WitRecognizer {
         // Instantiate a corresponding adapter
         if (cache) {
             // By default, key's will expire after 3 hours
-            const expire = typeof options.expire === 'number' ? options.expire : 3 * 3600;
+            const expire = typeof options.expire === "number" ? options.expire : 3 * 3600;
             const clientType = this.getClientType(cache);
 
             if (clientType !== CacheClients.Unknown) {
@@ -116,7 +61,8 @@ export class WitRecognizer {
                     this.cacheAdapter = new MemcachedAdapter(cache, expire);
                     break;
                 default: // CacheClients.Unknown
-                    throw new Error("Invalid cache client. View the module's README.md for more details => https://github.com/sebsylvester/botbuilder-wit/blob/master/README.md");
+                    const msg = "Invalid cache client. See the module's README.md for more details";
+                    throw new Error(msg);
             }
         }
     }
@@ -126,13 +72,13 @@ export class WitRecognizer {
      * @param {IRecognizeContext} context - contains the message received from the user
      * @param {function} done - the result callback
      */
-    recognize(context: IRecognizeContext, done: (err: Error, result: IIntentRecognizerResult) => void) {
-        let result = <IIntentRecognizerResult>{ score: 0.0, intent: null };
+    public recognize(context: IRecognizeContext, done: (err: Error, result: IIntentRecognizerResult) => void) {
+        const result = { score: 0.0, intent: null } as IIntentRecognizerResult;
         if (context && context.message && context.message.text) {
             const utterance = context.message.text;
 
             // Send a request to Wit.ai
-            this.witClient.message(utterance, <WitContext>{})
+            this.witClient.message(utterance, {} as WitContext)
                 .then((response: IWitResults) => {
                     // Check if Wit.ai responded with an error
                     if (response.error) {
@@ -142,7 +88,7 @@ export class WitRecognizer {
                     // Wit.ai currently does not support multiple intents.
                     // The intent property is either undefined or an array with a single object.
                     // Also note that, unlike LUIS.ai, Wit.ai treats an intent like a regular entity
-                    let { intent, ...entities } = response.entities;
+                    const { intent, ...entities } = response.entities;
                     const hasOtherEntities = Object.keys(entities).length > 0;
 
                     // If there no useful response from Wit.ai, trigger the IntentDialog's default handler
@@ -167,26 +113,26 @@ export class WitRecognizer {
                         // Setting score to 0.1 lets the intent still be triggered but keeps it from
                         // stomping on other models.
                         if (!result.intent) {
-                            result.intent = 'none';
+                            result.intent = "none";
                             result.score = 0.1;
                         }
 
                         result.entities = [];
-                        for (let key in entities) {
-                            for (let entity of entities[key]) {
+                        for (const key of Object.keys(entities)) {
+                            for (const entity of entities[key]) {
                                 const { type, value, confidence } = entity;
-                                const foundEntity = <IEntity>{
+                                const foundEntity = {
                                     type: key,
                                     entity: null, // default value
-                                    rawEntity: <Object>entity,
+                                    rawEntity: entity as {[key: string]: any},
                                     score: confidence,
-                                };
+                                } as IEntity;
 
                                 // In most cases the entity's in the response from Wit.ai will be of type "value".
                                 // There are other possible types, like "interval", in which case there will be a
                                 // "values" property instead. To deal with this variety of structures, there's a
                                 // "rawEntity" property to allow for custom entity processing in your own code.
-                                if (type === 'value') {
+                                if (type === "value") {
                                     // Overwrite the default value null, with a value that must be a string.
                                     foundEntity.entity = value;
                                     // The startIndex and endIndex values will not always be useful.
@@ -214,15 +160,15 @@ export class WitRecognizer {
      * @param {*} client - the client with which the results will be saved to the cache.
      * @returns {CacheClients} any of the possible enum values
      */
-    getClientType(client: any): CacheClients {
+    public getClientType(client: any): CacheClients {
         let clientType = CacheClients.Unknown;
 
-        if (typeof client === 'object' && client.constructor) {
+        if (typeof client === "object" && client.constructor) {
             switch (client.constructor.name) {
-                case 'RedisClient':
+                case "RedisClient":
                     clientType = CacheClients.Redis;
                     break;
-                case 'Client':
+                case "Client":
                     clientType = CacheClients.Memcached;
             }
         }
@@ -235,14 +181,13 @@ export class WitRecognizer {
      * @param {function} message - the original Wit.message function that will be decorated
      * @returns {function} the decorated function
      */
-    witDecorator(message: Function): (utterance: string) => any {
-        return (utterance) => {
+     private witDecorator(message: MessageHandler): MessageHandler {
+        return (utterance, context) => {
             // Create hash from the utterance.
-            const hash = crypto.createHash('sha256');
-            hash.update(utterance);
-            const key = hash.digest('hex');
+            const hash = createHash("sha256").update(utterance);
+            const key = hash.digest("hex");
 
-            return new Promise((resolve, reject) => {
+            return new Promise<MessageResponse>((resolve, reject) => {
                 // Check if the key exists
                 this.cacheAdapter.get(key, (error, result) => {
                     if (error) {
@@ -259,28 +204,28 @@ export class WitRecognizer {
                         resolve(null);
                     }
                 });
-            }).then(result => {
+            }).then((result) => {
                 if (result) {
                     // Reset expire to initial value
                     this.cacheAdapter.touch(key, (error, result) => {
-                        if (error) console.error(error);
+                        if (error) { console.error(error); }
                     });
                     // Return the result, skip Wit.ai
                     return Promise.resolve(result);
                 } else {
                     // No cached result was found, use Wit.ai to parse the utterance
-                    const witPromise = message(utterance);
+                    const witPromise = message(utterance, context);
                     witPromise.then((result: IWitResults) => {
                         // Cache the result from Wit.ai unless it contains an error message
                         // Possible error would be: "Bad auth, check token/params"
                         if (!result.error) {
                             const value = JSON.stringify(result);
                             this.cacheAdapter.set(key, value, (error, result) => {
-                                if (error) console.error(error);
+                                if (error) { console.error(error); }
                             });
                         }
                     }).catch((error: Error) => {
-                        console.error(error)
+                        console.error(error);
                     });
 
                     return witPromise;
